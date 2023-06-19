7e6bfd64210c8cb441571697da659aa44a8113ff
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-19 11:40:51 -0500

    implemented a function to create divs for the etch a sketch

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+const createGrid = function() {
+  let n = 16;
+  const container = document.querySelector('.container');
+  for(let i = 0; i < n; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div);
+    
+  };
+};
+
+createGrid();
+
