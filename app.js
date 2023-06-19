1b99aefcdbd9ff36cbbe5d29eaff87154344220d
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-19 14:16:31 -0500

    tying to add  a hover effect with javascript

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const createGrid = function() {
   const container = document.querySelector('.container');
   for(let i = 0; i < res; i++) {
     const div = document.createElement('div');
+    div.setAttribute('class', 'divs');
     container.appendChild(div);
     
   };
   container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+  const boxes = document.querySelectorAll('.divs')
+  boxes.forEach((box) => {
+    box.addEventListener('onmouseover', () => {
+      box.style.backgroundColor = 'green';
+    })
+  })
 };
 
 createGrid();
