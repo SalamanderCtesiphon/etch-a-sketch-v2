88e32c163a11b57d02f6d91da67ca9855b209f40
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-19 12:04:51 -0500

    implemented the logic to render the sketch pad

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 
 
 const createGrid = function() {
-  let n = 16;
+  let n = 10;
+  let res = n * n;
   const container = document.querySelector('.container');
-  for(let i = 0; i < n; i++) {
+  for(let i = 0; i < res; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
     
   };
+  container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
 };
 
 createGrid();
+
 
