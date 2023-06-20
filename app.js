021445b8cdc37ca27b7e3ae616da116b0288a338
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-20 08:54:35 -0500

    trying to add event listeners to the rendered divs

--- app.js
+++ app.js
@@ -5,19 +5,14 @@ const createGrid = function() {
   let res = n * n;
   const container = document.querySelector('.container');
   for(let i = 0; i < res; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'divs');
-    container.appendChild(div);
+    const sqrs = document.createElement('div');
+    sqrs.setAttribute('class', 'squares');
+    container.appendChild(sqrs);
     
   };
   container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
-  const boxes = document.querySelectorAll('.divs')
-  boxes.forEach((box) => {
-    box.addEventListener('onmouseover', () => {
-      box.style.backgroundColor = 'green';
-    })
-  })
+
 };
 
 createGrid();
